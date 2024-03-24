6857932243b57104af484a51d2c16189e3b69a95
unknown <agiljibrin009@gmail.com>
2024-03-24 21:02:37 +0800

    fix zlib

--- 31_zlib/index.mjs
+++ 31_zlib/index.mjs
@@ -5,3 +5,9 @@ const source = fs.readFileSync('31_zlib/catatan.txt');
 const result = zlib.gzipSync(source);
 
 fs.writeFileSync("31_zlib/kompresi/zlib.mjs.txt", result);
+
+// zlib decompres
+const decom = fs.readFileSync('31_zlib/kompresi/zlib.mjs.txt');
+const results = zlib.unzipSync(decom);
+
+console.info(results.toString());
